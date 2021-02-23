d02faf13d59ee350f040e6655c0ab4aa3d23d1e0
Alisher Karim <alisherktlwik@gmail.com>
2021-02-23 14:51:25 +0000

    add routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/apple', function () {
+    return view('apple');
+});
+Route::get('/bota', function () {
+    return "This is an example route!";
+});
